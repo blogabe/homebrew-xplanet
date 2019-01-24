0235ad1acc31c45be561291561680bcfb2ae8721
blogabe <6888675+blogabe@users.noreply.github.com>
2019-01-24 13:10:40 -0800

    remove bottle info

--- pygtk.rb
+++ pygtk.rb
@@ -5,14 +5,6 @@ class Pygtk < Formula
   sha256 "cd1c1ea265bd63ff669e92a2d3c2a88eb26bcd9e5363e0f82c896e649f206912"
   revision 2
 
-  bottle do
-    cellar :any
-    sha256 "6aa04785bcbf9c1e79a268b27263ce61dbfa64cb21023c426b70ead3b6671689" => :mojave
-    sha256 "228a919cbba58afc99747e9b5dc6d01013ef8463de2ede5d81c20103afccbb9f" => :high_sierra
-    sha256 "fce76d2bf1e1748ac110aede98a622e5f8b737390a3a5e22f56872834b73c033" => :sierra
-    sha256 "fd1cef5484267e02971c4daa1eda42e3a66c77786923b0f76b496007282b10a1" => :el_capitan
-  end
-
   depends_on "pkg-config" => :build
   depends_on "atk"
   depends_on "glib"
