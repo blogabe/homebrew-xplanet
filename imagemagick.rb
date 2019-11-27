004a3912bc8881a009931269674b8914943002be
blogabe <6888675+blogabe@users.noreply.github.com>
2019-11-27 07:19:25 -0800

    ImageMagick-7.0.9-6

--- imagemagick.rb
+++ imagemagick.rb
@@ -4,9 +4,9 @@ class Imagemagick < Formula
   # Please always keep the Homebrew mirror as the primary URL as the
   # ImageMagick site removes tarballs regularly which means we get issues
   # unnecessarily and older versions of the formula are broken.
-  url "https://dl.bintray.com/homebrew/mirror/ImageMagick-7.0.9-5.tar.xz"
-  mirror "https://www.imagemagick.org/download/ImageMagick-7.0.9-5.tar.xz"
-  sha256 "f6c1962c67e046e8899b6f7ca5b49c0de5795eee80c5f35b274d80e99b83aec8"
+  url "https://dl.bintray.com/homebrew/mirror/ImageMagick-7.0.9-6.tar.xz"
+  mirror "https://www.imagemagick.org/download/ImageMagick-7.0.9-6.tar.xz"
+  sha256 "2f1826cae724154a3da6679ef26981fcfc658c445b74ac1a75a122f708a425bd"
   head "https://github.com/ImageMagick/ImageMagick.git"
 
   option "with-fftw", "Compile with FFTW support"
