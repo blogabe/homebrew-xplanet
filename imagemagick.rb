8fb787e28f8c7ef6698e6a3128b16070d729672e
blogabe <6888675+blogabe@users.noreply.github.com>
2020-12-19 09:33:33 -0800

    ImageMagick-7.0.10-51

--- imagemagick.rb
+++ imagemagick.rb
@@ -4,9 +4,9 @@ class Imagemagick < Formula
   # Please always keep the Homebrew mirror as the primary URL as the
   # ImageMagick site removes tarballs regularly which means we get issues
   # unnecessarily and older versions of the formula are broken.
-  url "https://dl.bintray.com/homebrew/mirror/ImageMagick-7.0.10-50.tar.xz"
-  mirror "https://www.imagemagick.org/download/releases/ImageMagick-7.0.10-50.tar.xz"
-  sha256 "8ae6252938ecd11b7027aeb037b6bfa2280ea6d27ec939ec0ce40d1dc20b0e50"
+  url "https://dl.bintray.com/homebrew/mirror/ImageMagick-7.0.10-51.tar.xz"
+  mirror "https://www.imagemagick.org/download/releases/ImageMagick-7.0.10-51.tar.xz"
+  sha256 "ec7b15fd7310327feb9e325d3f0b23723c9a647470439f2a573b18a173db2399"
   license "ImageMagick"
   head "https://github.com/ImageMagick/ImageMagick.git"
 
