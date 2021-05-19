139e2234f9c0429f19b5697fbdc34cc231822b18
blogabe <6888675+blogabe@users.noreply.github.com>
2021-05-18 20:08:41 -0700

    ImageMagick-7.0.11-13

--- imagemagick.rb
+++ imagemagick.rb
@@ -1,8 +1,8 @@
 class Imagemagick < Formula
   desc "Tools and libraries to manipulate images in many formats"
   homepage "https://www.imagemagick.org/"
-  url "https://dl.bintray.com/homebrew/mirror/ImageMagick-7.0.11-12.tar.xz"
-  sha256 "8d43bb409fb8b31184e7345429b4481a7ee0353dca0048d56e8782b79790bbea"
+  url "https://dl.bintray.com/homebrew/mirror/ImageMagick-7.0.11-13.tar.xz"
+  sha256 "11797503bc4dd72c7e65e60783e156fd74ec0a551d2f1fe1f41eaa4bd06987a5"
   license "ImageMagick"
   head "https://github.com/ImageMagick/ImageMagick.git"
 
